b018fae04ff533c823cf66ea9ffe962e49b3d5e6
tcm5343 <48961675+tcm5343@users.noreply.github.com>
2024-11-05 00:13:51 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://saram7619.github.io/blog",
+  base: "https://tcm5343.github.io/blog",
   plugins: [react()],
 })
